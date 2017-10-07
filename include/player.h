ab4386649557671a3e334eb6ff80570f01efda9d
Murgalha <murilobaldi1@gmail.com>
2017-10-07 16:50:38 -0300

    Add header files

---
+++ include/player.h
@@ -0,0 +1,24 @@
+#ifndef _PLAYER_H_
+#define _PLAYER_H_
+
+#include <ncurses.h>
+
+typedef struct {
+	int x, y;
+} POINT;
+
+typedef struct {
+	char going;
+	POINT *body;
+	int body_size;
+	int score;
+	POINT last_pos;
+} PLAYER;
+
+PLAYER *create_player(int, int);
+void destroy_player(PLAYER *);
+void move_player(PLAYER *);
+void change_direction(PLAYER *, char);
+void draw_player(WINDOW *, PLAYER *);
+
+#endif
---
+++ include/window.h
@@ -0,0 +1,9 @@
+#ifndef _WINDOW_H_
+#define _WINDOW_H_
+
+#define SCORE_SIZE 1
+
+void resize_window(WINDOW *, WINDOW *, int *, int *, int *, int *);
+void draw_borders(WINDOW *);
+
+#endif
